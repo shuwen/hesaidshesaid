9f8d797bde1875340c0c835432762c63ba3afe65
celsowhite <celso.white13@gmail.com>
2015-08-15 17:07:38 -0500

    First Commit. Introduction screens

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,12 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: "fade",
+    controlNav: false,
+    directionNav: false,
+    slideshowSpeed: 2000
+  });
+});
