4076021e3045eb1fd5a67dbd67d21ff60e01c4e3
celsowhite <celso.white13@gmail.com>
2015-08-15 23:20:04 -0500

    Updated stylesheets and introduction.

--- app/js/app.js
+++ app/js/app.js
@@ -2,6 +2,16 @@
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
 
+$(document).ready(function() {
+
+	$('#bgvid').on('ended',function() {    
+
+		$( "#choose_your_story" ).load( "chooseyourstory.html" );
+
+	});
+
+});
+
 $(window).load(function() {
   $('.flexslider').flexslider({
     animation: "fade",
